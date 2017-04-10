ff62e05a2392ffaa025ee3b5a956bc0fe41cd06c
Ed M Barwani <ed.barwani@gmail.com>
2017-04-11 02:43:12 +1000

    Implement fmt.Stringer on the AST
    This is mainly to help with debugging and testing.

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -5,10 +5,16 @@
 // are expressions.
 package ast
 
-import "github.com/emb/play/monkey/token"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/emb/play/monkey/token"
+)
 
 // Node an interface describing a node in the AST.
 type Node interface {
+	fmt.Stringer
 	TokenLiteral() string
 }
 
@@ -35,6 +41,19 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+// String constructs a string representing the program
+// statements. Useful for debugging and testing.
+func (p *Program) String() string {
+	if p == nil {
+		return ""
+	}
+	var buf bytes.Buffer
+	for _, s := range p.Statements {
+		buf.WriteString(s.String())
+	}
+	return buf.String()
+}
+
 // LetStmt describes a Let statement.
 type LetStmt struct {
 	Token token.Token
@@ -44,6 +63,14 @@ type LetStmt struct {
 
 // TokenLiteral returns the token literal underlying let statement.
 func (l *LetStmt) TokenLiteral() string { return l.Token.Literal }
+
+// String reconstructs let statement into valid code.
+func (l *LetStmt) String() string {
+	if l == nil {
+		return ""
+	}
+	return fmt.Sprintf("%s %s = %s;", l.TokenLiteral(), l.Name, l.Value)
+}
 
 // Identifier describes user defined names used during variable
 // bindings in the language.
@@ -55,6 +82,14 @@ type Identifier struct {
 // TokenLiteral returns the literal value of an identifier token.
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+// String returns the value of identifier
+func (i *Identifier) String() string {
+	if i == nil {
+		return ""
+	}
+	return i.Value
+}
+
 // ReturnStmt describes a Return statement.
 type ReturnStmt struct {
 	Token token.Token
@@ -63,6 +98,14 @@ type ReturnStmt struct {
 
 // TokenLiteral returns the token literal underlying return statement.
 func (r *ReturnStmt) TokenLiteral() string { return r.Token.Literal }
+
+// String reconstructs return statement into valid code.
+func (r *ReturnStmt) String() string {
+	if r == nil {
+		return ""
+	}
+	return fmt.Sprintf(" %s %s;", r.TokenLiteral(), r.Value)
+}
 
 // ExpressionStmt describes an Expression statement. Unlike the main two
 // statements of the language this is a wrapper. Since the following
@@ -78,3 +121,11 @@ type ExpressionStmt struct {
 // TokenLiteral returns the literal value of the first token in an
 // expression.
 func (e *ExpressionStmt) TokenLiteral() string { return e.Token.Literal }
+
+// String reconstruct an Expression statement into valid code.
+func (e *ExpressionStmt) String() string {
+	if e == nil {
+		return ""
+	}
+	return e.Expression.String()
+}
