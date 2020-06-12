39fd465a86052f3988c41dc80ff63fad50254b8d
Ed Barwani <ed.barwani@gmail.com>
2020-06-12 08:12:21 -0700

    nand2tetris/hackvmt: Implement a subset of the VM commands
    Expand a rough implementation of the arithmetic commands and memory
    access command. This implementation is ugly and requires some
    thinking. But it works for now.

--- nand2tetris/hackvmt/translator.go
+++ nand2tetris/hackvmt/translator.go
@@ -8,9 +8,12 @@ import (
 
 // Translate converts VM code to assembly code.
 func Translate(ch <-chan *Command, w io.Writer) error {
+	// unique is useful to add for any labels that maybe used.
+	// there is no semantic
+	unique := 0
 	// channel gets closed when we finish reading.
 	for cmd := range ch {
-		translated, err := translate(cmd)
+		translated, err := translate(cmd, unique)
 		if err != nil {
 			return err
 		}
@@ -25,18 +28,19 @@ func Translate(ch <-chan *Command, w io.Writer) error {
 		if err != nil {
 			return err
 		}
+		unique++
 	}
 	return nil
 }
 
-func translate(c *Command) ([]byte, error) {
+func translate(c *Command, unique int) ([]byte, error) {
 	switch c.kind {
 	case CmdPush:
 		return push(c.arg, *c.param)
 	case CmdPop:
 		return pop(c.arg, *c.param)
 	case CmdArithmetic:
-		return arith(c.arg)
+		return arith(c.arg, unique)
 	}
 	return nil, fmt.Errorf("translate: unsupported command %s", c)
 }
@@ -50,8 +54,35 @@ const pushFrag = `	@SP
 	M=M+1
 `
 
+var addrMap = map[string]string{
+	"local":    "LCL",
+	"argument": "ARG",
+	"this":     "THIS",
+	"that":     "THAT",
+	"temp":     "R5", // Address Number 5
+}
+
 func push(segment string, index int) ([]byte, error) {
 	switch segment {
+	case "local", "argument", "this", "that":
+		addr := addrMap[segment]
+		buf := bytes.NewBufferString(fmt.Sprintf("\t@%d\n", index))
+		buf.WriteString("\tD=A\n")
+		buf.WriteString(fmt.Sprintf(`	@%s
+	A=M+D
+	D=M
+`, addr))
+		buf.WriteString(pushFrag)
+		return buf.Bytes(), nil
+	case "temp":
+		buf := bytes.NewBufferString(fmt.Sprintf("\t@%d\n", index))
+		buf.WriteString("\tD=A\n")
+		buf.WriteString(`	@5
+	A=D+A
+	D=M
+`)
+		buf.WriteString(pushFrag)
+		return buf.Bytes(), nil
 	case "constant":
 		buf := bytes.NewBufferString(fmt.Sprintf("\t@%d\n", index))
 		buf.WriteString("\tD=A\n")
@@ -62,29 +93,126 @@ func push(segment string, index int) ([]byte, error) {
 }
 
 // pop fragment is a hack assembly fragment that pops an item of the
-// stack into M
+// stack into D
 const popFrag = `	@SP
 	AM=M-1
+	D=M
 `
 
 func pop(segment string, index int) ([]byte, error) {
+	switch segment {
+	case "local", "argument", "this", "that":
+		addr := addrMap[segment]
+		buf := bytes.NewBufferString(fmt.Sprintf("\t@%d\n", index))
+		buf.WriteString("\tD=A\n")
+		// Store address in R13
+		buf.WriteString(fmt.Sprintf(`	@%s
+	D=M+D
+	@R13
+	M=D
+`, addr))
+		buf.WriteString(popFrag)
+		buf.WriteString(`	@R13
+	A=M
+	M=D
+`)
+		return buf.Bytes(), nil
+	case "temp":
+		if index > 8 {
+			return nil, fmt.Errorf("translate: index % out of bound for temp memory segment", index)
+		}
+		buf := bytes.NewBufferString(fmt.Sprintf("\t@%d\n", index))
+		buf.WriteString(`	D=A
+	@5
+	D=D+A
+	@R14
+	M=D
+`)
+		buf.WriteString(popFrag)
+		buf.WriteString(`	@R14
+	A=M
+	M=D`)
+		return buf.Bytes(), nil
+	}
 	return nil, fmt.Errorf("translate: pop unsupported memory segment %q", segment)
 }
 
-func arith(op string) ([]byte, error) {
+func arith(op string, unique int) ([]byte, error) {
 	switch op {
 	case "add":
 		// Pop into D
 		buf := bytes.NewBufferString(popFrag)
-		buf.WriteString("\tD=M\n")
 		// We don't have to pop here since we have a pointer
 		// to the memory address we can manipulate it directly
 		// rather than pop/push. We can add directly.
 		buf.WriteString(`	@SP
 	A=M-1
-	M=D+M
+	M=M+D
 `)
 		return buf.Bytes(), nil
+	case "sub":
+		// Pop into D
+		buf := bytes.NewBufferString(popFrag)
+		buf.WriteString(`	@SP
+	A=M-1
+	M=M-D
+`)
+		return buf.Bytes(), nil
+	case "neg":
+		// Modify top of the stack with
+		return []byte(`	@SP
+	A=M-1
+	M=-M
+`), nil
+	case "eq":
+		return branch("EQ", unique), nil
+	case "lt":
+		return branch("LT", unique), nil
+	case "gt":
+		return branch("GT", unique), nil
+	case "and":
+		buf := bytes.NewBufferString(popFrag)
+		buf.WriteString(`	@SP
+	A=M-1
+	M=D&M
+`)
+		return buf.Bytes(), nil
+	case "or":
+		buf := bytes.NewBufferString(popFrag)
+		buf.WriteString(`	@SP
+	A=M-1
+	M=D|M
+`)
+		return buf.Bytes(), nil
+	case "not":
+		return []byte(`	@SP
+	A=M-1
+	M=!M
+`), nil
 	}
 	return nil, fmt.Errorf("translate: unsupported arithmetic command %q", op)
+}
+
+// branch creates a branching fragment based on t, t must be one of EQ/LT/GT.
+func branch(t string, unique int) []byte {
+	branch := fmt.Sprintf("IF.%s.%d", t, unique) // e.g. IF.LT.1
+	branchEnd := fmt.Sprintf("IF.END.%d", unique)
+	buf := bytes.NewBufferString(popFrag)
+	buf.WriteString(fmt.Sprintf(`	@SP
+	A=M-1
+	D=M-D
+	@%s
+	D;J%s
+	@SP
+	A=M-1
+	M=0    // false
+	@%s
+	0;JMP
+(%s)
+	@SP
+	A=M-1
+	M=-1   // true
+(%s)
+`, branch, t, branchEnd, branch, branchEnd))
+	return buf.Bytes()
 }
